54361cd334ef8f0165fcb999d7a46994ed38667f
Ajay Singh <ajaysingh942@gmail.com>
2023-06-07 13:02:41 +0530

    (Mathmatics program) : SieveOfEratosthenes added

---
+++ LearnCoding/Mathematics/SieveOfEratosthenes.java
@@ -0,0 +1,24 @@
+//Problem: https://practice.geeksforgeeks.org/problems/sieve-of-eratosthenes5242/1
+
+class Solution{
+    static ArrayList<Integer> sieveOfEratosthenes(int N){
+        boolean prime[] = new boolean[N+1];
+        Arrays.fill(prime,true);
+
+        for(int p=2; p*p<=N; p++) {
+            if(prime[p]) {
+                for(int j=p*p; j<=N; j+=p) {
+                    prime[j] = false;
+                }
+            }
+        }
+
+        ArrayList<Integer> primeList = new ArrayList();
+        for(int i=2; i<=N; i++) {
+            if(prime[i]) {
+                primeList.add(i);
+            }
+        }
+        return primeList;
+    }
+}
