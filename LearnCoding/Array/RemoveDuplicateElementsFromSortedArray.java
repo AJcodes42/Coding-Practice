6f4808dc743ee444f8c8ffac612b4687b336fecb
Ajay Singh <ajaysingh942@gmail.com>
2023-06-08 11:49:24 +0530

    (new solution Array): RemoveDuplicateElements

--- LearnCoding/Array/RemoveDuplicateElementsFromSortedArray.java
+++ LearnCoding/Array/RemoveDuplicateElementsFromSortedArray.java
@@ -13,4 +13,14 @@ class Solution {
         A[resultLength] = A[A.length-1];
         return resultLength+1;
     }
+
+    int remove_duplicate2(int A[],int N){
+        int index =1;
+        for(int i=0; i<A.length - 1; i++) {
+            if(A[i]!=A[i+1]) {
+                A[index++] = A[i+1];
+            }
+        } 
+        return index;
+    }
 }
