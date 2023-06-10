cc4499350702430349a7234f0aae774f9b13bdc8
Ajay Singh <ajaysingh942@gmail.com>
2023-06-10 15:03:32 +0530

    (Search program added): CeilingInSortedArray

---
+++ LearnCoding/SearchingAndSorting/CeilingInSortedArray.java
@@ -0,0 +1,25 @@
+//Problem: https://www.geeksforgeeks.org/ceiling-in-a-sorted-array/
+public class Solution {
+    static int ceiling(int[] arr, int target) {
+
+        //If the target is greater than the greatest number in the array
+        if (target > arr[arr.length - 1]) {
+            return -1;
+        }
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return start;
+    }
+}
